5cc66f896a504d5c75226da8cf0a36a922abdd28
unknown <=>
2011-10-22 01:41:14 -0400

    initiali commit of cli.rb

--- cli.rb
+++ cli.rb
@@ -1,28 +1,49 @@
 require 'net/http'
 require 'uri'
-
+require 'nokogiri'
+require 'open-uri'
+# This is the file where javascript will be stored locally on our machine.
+$outputFileName = "symtax.js"
 
 class HttpTest
 	def downloadWebPage(weburl)
-		http_response = Net::HTTP.get_response(URI.parse(weburl))
-		puts http_response.body
+		#http_response = Net::HTTP.get_response(URI.parse(weburl))
+		
+		outfile = File.new($outputFileName.to_s,"w")
+		# Use Nokogiri library to write javascript into
+		# our file
+		# For now, we create a document, and just look for the script tags...
+		doc = Nokogiri::HTML(open(weburl))
+		doc.css('script').each do |script|
+			outfile << script.content #http_response.body #if we use http_response.body we get everything!
+			puts 
+		end
+		
+		outfile.close() # Close our file because we're done
 	end
 end
 
 if ARGV[0].match(/^http/) 
-	
+	# Load up a http page here
 	puts "is an http url"
-	test = HttpTest.new
+	test = HttpTest.new # Create a new instance of our class
+						# to load our webpage in.
 	test.downloadWebPage(ARGV[0].to_s)	
-	# url = URL.parse('http://www.google.com/index.html')
-	# res = Net::HTTP.start(url.host,url.port){|http|
-	# http.get('/index.html')
-	# }
-elsif ARGV[0].match(/^www/)
+elsif ARGV[0].match(/^www/)	
+	# Same code as above, but appends a http:// to the file
+	# This saves us an error message...
 	puts "is a www url"
 	test = HttpTest.new
 	test.downloadWebPage("http://"+ARGV[0].to_s)
-else
-	puts "not a url"
+else # Load a local file and save it into our symtax.js
+	 # since we are confident that our users are loading javascript
+	 # we do not pass it through nokogiri in our webpage class.
+		inputFile = File.new(ARGV[0].to_s,"r")
+			outfile = File.new($outputFileName.to_s,"w")
+				inputFile.each{|i|
+								outfile.write i
+				}
+			outfile.close()
+		inputFile.close()
 end
 
