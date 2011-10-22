c5d5593a54e12eac575d7c87b46e609e00edf4a7
Eli <eli@elilies.com>
2011-10-22 03:38:25 -0400

    awwww yeah

--- conductor.rb
+++ conductor.rb
@@ -9,30 +9,27 @@ class Conductor
     @max_height = max_height
   end
   
-  def enqueue(name, notes, octave, root, total_duration, height)
-      sleep_time = 0
-      elapsed = 0
-      notes.cycle do |(offset, duration)|
-        break if elapsed + duration > total_duration
+  def enqueue(name, notes, octave, root, total_duration, delay, height)
+    sleep_time = 0
+    elapsed = 0
+    notes.cycle do |(offset, duration)|
+      break if elapsed + duration > total_duration
 
-        @q << [elapsed, name, {freq: freq(octave, root, offset), dur: duration}]
-          
-        elapsed += duration
-      end
+      @q << [elapsed+delay, name, {freq: freq(octave, root, offset), dur: duration}]
+        
+      elapsed += duration
+    end
   end
   
   def commit 
     elapsed = 0
-    for (start, name, opts) in @q.sort_by(&:first)
+    for (start, name, opts) in @q.sort_by {|x| x[0]}
       puts "Synthing #{name} @ #{start}"
       
       sleep(start - elapsed)
       elapsed = start
       
-      Synth.new(name)
-      # TODO: deal with note, duration pairs
-      #       deal with rests
-      #Synth.new(name, opts)
+      Synth.new(name, opts)
     end
   end
   
