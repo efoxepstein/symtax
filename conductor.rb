63feeb482011676999077915b5cf7b9efe2329ed
Eli <eli@elilies.com>
2011-10-22 04:25:57 -0400

    amplitudal

--- conductor.rb
+++ conductor.rb
@@ -4,9 +4,13 @@ class Conductor
     (12*octave + root + offset).freq
   end
   
+  def amp(height)
+    [(Math.log(1.1-height/@max_height)+4)/4, 1].min
+  end
+  
   def initialize(max_height)
     @q = []
-    @max_height = max_height
+    @max_height = max_height.to_f
   end
   
   def enqueue(name, notes, octave, root, total_duration, delay, height)
@@ -16,7 +20,9 @@ class Conductor
       break if elapsed + duration > total_duration
       
       unless offset == nil
-        @q << [elapsed+delay, name, {freq: freq(octave, root, offset), dur: duration}]
+        @q << [elapsed+delay, name, { freq: freq(octave, root, offset), 
+                                      dur:  duration,
+                                      amp:  amp(height)}]
       end
       
       elapsed += duration
@@ -26,7 +32,7 @@ class Conductor
   def commit 
     elapsed = 0
     for (start, name, opts) in @q.sort_by {|x| x[0]}
-      puts "Synthing #{name} @ #{start}"
+      puts "Synthing #{name} @ #{start} / #{opts}"
       
       sleep(start - elapsed)
       elapsed = start
