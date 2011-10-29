7267ed77780b2e0a8a6f838280b4efc5870c8320
Eli <eli@elilies.com>
2011-10-28 23:22:24 -0400

    Update stuff

--- test.js
+++ test.js
@@ -3,8 +3,9 @@
   x.technology = function(){
     return "Ruby (1.9.2, hand-patched SCRuby, RKelly, FSSM), SuperCollider 3";
   }
-  var we    = { win : function(){return true;} },
-      our   = { names: ["Eli", "Paul", "and", "Mike"], happiness: 5.0 };
+  
+  we  = { win : function(){ return true; } };
+  our = { names: ["Eli", "Paul", "and", "Mike"], happiness: 5.0 };
   
   if( we.win(this.competition) ){ our.happiness += 10; }
   
